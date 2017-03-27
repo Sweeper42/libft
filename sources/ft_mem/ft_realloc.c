8ee1e304bd2e290086a1f504356498212e7a394d
Nelson PERRIN <nperrin@e2r8p14.42.fr>
2017-03-27 13:21:10 +0200

    update ft_realloc

--- sources/ft_mem/ft_realloc.c
+++ sources/ft_mem/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: nperrin <nperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/21 18:10:55 by nperrin           #+#    #+#             */
-/*   Updated: 2017/03/21 18:22:19 by nperrin          ###   ########.fr       */
+/*   Updated: 2017/03/27 13:19:02 by nperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,20 @@ void			*ft_realloc(
 	void	*new;
 	size_t	cpy_size;
 
-	if (!(new = malloc(sizeof(new_size) * sizeof(char))))
-		return (NULL);
-	cpy_size = (old_size > new_size) ? new_size : old_size;
-	ft_memcpy(new, *mem_addr, cpy_size);
-	ft_bzero((void *)((char *)new + cpy_size), new_size - cpy_size);
-	free(*mem_addr);
+	if (old_size == new_size)
+		return (*mem_addr);
+	if (new_size > 0)
+	{
+		if (!(new = malloc(sizeof(new_size) * sizeof(char))))
+			return (NULL);
+		cpy_size = (old_size > new_size) ? new_size : old_size;
+		ft_memcpy(new, *mem_addr, cpy_size);
+		ft_bzero((void *)((char *)new + cpy_size), new_size - cpy_size);
+	}
+	else
+		new = NULL;
+	if (old_size > 0)
+		free(*mem_addr);
 	*mem_addr = new;
 	return (new);
 }
